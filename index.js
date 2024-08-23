a0f026be6c3fe1d4076b51da4722bd8de321f799
rahul pandit <87419125+rahulll1123@users.noreply.github.com>
2024-08-24 01:42:05 +0530

    update

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs');
+
+
 
 app.use(express.static('public'))
 // const bodyParser = require('body-parser');
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html',{root:__dirname})
+  res.sendFile('index.html', { root: __dirname })
 })
 
 app.get('/pages/:page', (req, res) => {
-  res.sendFile(__dirname+`/pages/${req.params.page}`)
+  res.sendFile(__dirname + `/pages/${req.params.page}`)
 })
 
 app.post('/submit', (req, res) => {
   console.log("its a post request")
-  const {...data}=req.body
-  console.log(data)
-  
-  res.sendFile('index.html',{root:__dirname})
+  const { ...recieved_data } = req.body
+  // console.log(recieved_data)
+
+  var data = fs.readFileSync("data/companies.json");
+  var data_json = JSON.parse(data);
+
+  data_json.push(recieved_data);
+
+  var data_string = JSON.stringify(data_json);
+  fs.writeFile("data/companies.json", data_string, (err) => {
+    // Error checking
+    if (err) throw err;
+    console.log("New data added");
+  });
+
+  res.sendFile('pages/redirect.html', { root: __dirname })
 })
 
-app.listen(port, () => 
+app.post('/loadcard', (req, res) => {
+  var data = fs.readFileSync("data/companies.json");
+  // var data_json = JSON.parse(data);
+  // var data_string=JSON.stringify(data_json);
+  res.send(data)
+})
+
+app.listen(port, () =>
   console.log(`Example app listening on port ${port}`)
 )
