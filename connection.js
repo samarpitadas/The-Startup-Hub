020e3c514b8da9526d74724c268eee03e1735855
rahul pandit <87419125+rahulll1123@users.noreply.github.com>
2024-09-07 00:14:53 +0530

    ..

--- connection.js
+++ connection.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 async function connectdbs() {
-    const conn = await mongoose.connect("mongodb://localhost:27017/startup_data")
-    // const conn = await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}`)
+    // const conn = await mongoose.connect("mongodb://localhost:27017/startup_data")
+    const conn = await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}`)
 
 }
 
