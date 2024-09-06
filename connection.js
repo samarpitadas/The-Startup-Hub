013d0714947a33f15958a94fe426c7cafe343b24
rahul pandit <87419125+rahulll1123@users.noreply.github.com>
2024-09-06 18:34:53 +0530

    env added

--- connection.js
+++ connection.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 async function connectdbs() {
-    const conn = await mongoose.connect("mongodb+srv://raj902121raj:rahulpandit123@database.ox2us.mongodb.net/")
+    // const conn = await mongoose.connect("mongodb+srv://raj902121raj:rahulpandit123@database.ox2us.mongodb.net/")
+    const conn = await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}`)
+
 }
 
 export { connectdbs }
